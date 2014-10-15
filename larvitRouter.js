8a7e6bb28f34aa2c75dde607005514eb1117ad85
Lilleman <lilleman@larvit.se>
2014-10-15 11:22:25 +0200

    Major changes yo

--- larvitRouter.js
+++ larvitRouter.js
@@ -1,152 +1,134 @@
-var fs  = require('fs'),
-    url = require('url');
+	var fs    = require('fs'),
+	    url   = require('url')
+	    merge = require('utils-merge');
 
-exports.pubFilePath = './public';
-exports.tmplDir     = 'html';
+exports = module.exports = function(options) {
 
-// Todo: Go over this and make it more loosley coupled
-exports.tmplEngine  = require('larvittmpl');
+	// Copy options object
+	options = merge({
+		'pubFilePath':  './public',
+		'tmplDir':      'html',
+		'tmplEngine':   require('larvittmpl'),
+		'customRoutes': []
+	}, options);
 
-/**
- * Resolve what to do based on request
- *
- * Will populate or modify if it exists:
- * request.urlParsed
- * request.staticFilename - if there is a matching static file
- * request.controllerName - if there is a matching controller
- *
- * @param obj request
- * @param func callback(err, info)
- */
-exports.resolve = function(request, callback) {
-	request.urlParsed = url.parse(request.url, true);
-	var pathname      = request.urlParsed.pathname;
+	var returnObj = {};
 
-	// Remove .json path ending
-	if (pathname.substring(pathname.length - 5) == '.json')
-		pathname = pathname.substring(0, pathname.length - 5);
+	returnObj.resolve = function resolve(request, callback) {
+		request.urlParsed = url.parse(request.url, true);
+		var pathname      = request.urlParsed.pathname;
 
-	// Default route always is default
-	if (pathname == '/') {
-		request.controllerName = 'default';
-		callCallback();
-	}
+		// Remove .json path ending
+		if (pathname.substring(pathname.length - 5) == '.json')
+			pathname = pathname.substring(0, pathname.length - 5);
 
-	/* Do custom routes like this:
-	else if (RegExp('^/mupp$').test(pathname)) {
-		request.controllerName = 'muppet';
-		callCallback();
-	}
-	*/
-
-	else {
-
-		// Try to match a static file and if that fails, try to match a controller from URL
-		var thisPubFilePath = exports.pubFilePath + pathname;
-		fs.stat(thisPubFilePath, function(err, stat) {
-			if ( ! err && stat.isFile()) {
-				// File found! Set the staticFilename and call the callback
-				request.staticFilename = thisPubFilePath;
+		var i = 0;
+		while (options.customRoutes[i] !== undefined) {
+			if (RegExp(options.customRoutes[i].regex).test(pathname)) {
+				request.controllerName = options.customRoutes[i].controllerName;
 				callCallback();
-			} else {
-				// No static file was found, see if we have a matching controller when resolved from URL
-				var controllerName = pathname.substring(1);
+			}
+			i++;
+		}
 
-				//console.info('larvitRouter.js - resolve() auto-resolved non-confirmed controllerName: ' + controllerName);
+		if (request.controllerName === undefined) {
 
-				fs.stat('./controllers/' + controllerName + '.js', function(err, stat) {
-					if ( ! err && stat.isFile()) {
-						request.controllerName = controllerName;
-
-						//console.info('larvitRouter.js - resolve() confirmed auto-resolved controllerName: ' + controllerName);
-					}
+			// Try to match a static file and if that fails, try to match a controller from URL
+			var thisPubFilePath = options.pubFilePath + pathname;
+			fs.stat(thisPubFilePath, function(err, stat) {
+				if ( ! err && stat.isFile()) {
+					// File found! Set the staticFilename and call the callback
+					request.staticFilename = thisPubFilePath;
 					callCallback();
-				});
-			}
-		});
-	}
-
-	// Call callback if callable
-	function callCallback() {
-		if (typeof callback === 'function') {
-			if (request.controllerName === undefined && request.staticFilename === undefined) {
-				var errorMsg = 'larvitRouter.js - resolve() - Route "' + request.urlParsed.pathname + '" could not be resolved';
-				console.error(errorMsg);
-				callback(new Error(errorMsg));
-			} else {
-				callback(null);
-			}
-		}
-	}
-}
-
-/**
- * Send response to client
- *
- * @param obj err - Error object
- * @param obj request
- * @param obj response
- * @param obj data - Data to send
- */
-exports.sendToClient = function(err, request, response, data) {
-	if ( ! err) {
-		var splittedPath = request.urlParsed.pathname.split('.');
-
-		// We need to set the request type. Can be either json or html
-		if (splittedPath[splittedPath.length - 1] == 'json') {
-			request.type = 'json';
-			request.controllerName = request.controllerName.substring(0, request.controllerName.length - 5);
-			if (request.controllerName == '')
-				request.controllerName = 'default';
-		} else {
-			request.type = 'html';
-		}
-
-		if (request.type == 'html') {
-			var tmplName = '/' + exports.tmplDir + '/' + request.controllerName + '.html',
-			    tmplRequest = {'url': tmplName};
-
-			// Make an internal resolve for the template
-			// We do this to imitade what it would be to fetch the
-			// template from the clients perspective
-			exports.resolve(tmplRequest, function(err){
-				if ( ! err && tmplRequest.staticFilename !== undefined) {
-
-					fs.readFile(tmplRequest.staticFilename, function(err, tmplFileContent){
-						if ( ! err) {
-							exports.tmplEngine.render(tmplFileContent.toString(), data, function(err, htmlStr){
-								if ( ! err) {
-									if ( ! response.statusCode)
-										response.statusCode = 200;
-
-									response.writeHead(response.statusCode, {'Content-Type': 'text/html'});
-									response.end(htmlStr);
-								} else {
-									internalError(err);
-								}
-							});
-						} else {
-							internalError(err);
-						}
-					})
-
 				} else {
-					internalError(new Error('template "' + tmplName + '" not found'));
+					// No static file was found, see if we have a matching controller when resolved from URL
+					var controllerName = pathname.substring(1);
+
+					fs.stat('./controllers/' + controllerName + '.js', function(err, stat) {
+						if ( ! err && stat.isFile())
+							request.controllerName = controllerName;
+						callCallback();
+					});
 				}
 			});
-		} else if (request.type == 'json') {
-			response.writeHead(200, {'Content-Type': 'application/json'});
-			response.end(JSON.stringify(data));
 		}
-	} else {
-		internalError(err);
-	}
 
-	function internalError(err) {
-		console.error('larvitRouter.js - sendToClient() exited due to error:');
-		console.error(err);
+		// Call callback if callable
+		function callCallback() {
+			if (typeof callback === 'function') {
+				if (request.controllerName === undefined && request.staticFilename === undefined) {
+					var errorMsg = 'larvitRouter.js - resolve() - Route "' + request.urlParsed.pathname + '" could not be resolved';
+					console.error(errorMsg);
+					callback(new Error(errorMsg));
+				} else {
+					callback(null);
+				}
+			}
+		}
+	};
 
-		response.writeHead(500, {'Content-Type': 'text/plain'});
-		response.end('Internal server error');
-	}
+	returnObj.sendToClient = function sendToClient(err, request, response, data) {
+		if ( ! err) {
+			var splittedPath = request.urlParsed.pathname.split('.');
+
+			// We need to set the request type. Can be either json or html
+			if (splittedPath[splittedPath.length - 1] == 'json') {
+				request.type = 'json';
+				request.controllerName = request.controllerName.substring(0, request.controllerName.length - 5);
+				if (request.controllerName == '')
+					request.controllerName = 'default';
+			} else {
+				request.type = 'html';
+			}
+
+			if (request.type == 'html') {
+				var tmplName = '/' + options.tmplDir + '/' + request.controllerName + '.html',
+				    tmplRequest = {'url': tmplName};
+
+				// Make an internal resolve for the template
+				// We do this to imitade what it would be to fetch the
+				// template from the clients perspective
+				returnObj.resolve(tmplRequest, function(err){
+					if ( ! err && tmplRequest.staticFilename !== undefined) {
+
+						fs.readFile(tmplRequest.staticFilename, function(err, tmplFileContent){
+							if ( ! err) {
+								options.tmplEngine.render(tmplFileContent.toString(), data, function(err, htmlStr){
+									if ( ! err) {
+										if ( ! response.statusCode)
+											response.statusCode = 200;
+
+										response.writeHead(response.statusCode, {'Content-Type': 'text/html'});
+										response.end(htmlStr);
+									} else {
+										internalError(err);
+									}
+								});
+							} else {
+								internalError(err);
+							}
+						})
+
+					} else {
+						internalError(new Error('template "' + tmplName + '" not found'));
+					}
+				});
+			} else if (request.type == 'json') {
+				response.writeHead(200, {'Content-Type': 'application/json'});
+				response.end(JSON.stringify(data));
+			}
+		} else {
+			internalError(err);
+		}
+
+		function internalError(err) {
+			console.error('larvitRouter.js - sendToClient() exited due to error:');
+			console.error(err);
+
+			response.writeHead(500, {'Content-Type': 'text/plain'});
+			response.end('Internal server error');
+		}
+	};
+
+	return returnObj;
 }
